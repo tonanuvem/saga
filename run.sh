b4c60985afe44177f3426ad08afdca11c5458a74
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-12-01 14:33:49 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@ docker-compose up -d
 
 echo ""
 echo "Aguardando a configuração do NIFI."
-
 while [ "$(docker logs saga_nifi_1 2>&1 | grep "Controller initialization took" | wc -l)" != "1" ]; do
   printf "."
   sleep 1
