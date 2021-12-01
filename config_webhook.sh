61d427cde9548552dabc49ee3a57142fa81d7b27
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-12-01 09:58:32 -0300

    Update config_webhook.sh

--- config_webhook.sh
+++ config_webhook.sh
@@ -2,3 +2,4 @@ echo "Digite a URL que está no canal #webhook:"
 read WEBHOOK
 
 sed -i 's|inserir_webhook|'$WEBHOOK'|' ./producer/Dockerfile
+sed -i 's|inserir_webhook|'$WEBHOOK'|' docker-compose.yml
