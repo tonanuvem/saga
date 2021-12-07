1152a599c049a239a058e98089f204537c76c92f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-12-07 09:06:50 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,3 +9,12 @@ done
 echo ""
 echo "Config OK"
 echo ""
+IP=$(curl -s checkip.amazonaws.com)
+echo ""
+echo "URLs do projeto:"
+echo ""
+echo " - NIFI            : $IP:8090/nifi"
+echo " - PRODUCER        : $IP:5001"
+echo " - KAFKA UI        : $IP:8070"
+echo " - Mongo UI        : $IP:8081"
+echo ""
