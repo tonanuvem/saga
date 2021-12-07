81bf94cbec1f3080e6b5908b2c089866de77bd94
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-12-07 18:56:53 -0300

    Update cluster_run.sh

--- cluster_run.sh
+++ cluster_run.sh
@@ -1,1 +1,9 @@
 docker stack deploy -c docker-compose-swarm.yml saga
+echo ""
+echo "Portas disponíveis no projeto:"
+echo ""
+echo " - NIFI            : 8090/nifi"
+echo " - PRODUCER        : 5001"
+echo " - KAFKA UI        : 8070"
+echo " - Mongo UI        : 8081"
+echo ""
