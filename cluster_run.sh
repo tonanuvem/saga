c6f57d660434bd108269381e8c59c0bf3d43e61b
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-12-08 11:55:57 -0300

    Update cluster_run.sh

--- cluster_run.sh
+++ cluster_run.sh
@@ -1,5 +1,11 @@
 docker stack deploy -c docker-compose-swarm.yml saga
 echo ""
+echo "Aguardando a configuração do NIFI."
+while [ "$(docker service logs saga_nifi 2>&1 | grep "Controller initialization took" | wc -l)" != "1" ]; do
+  printf "."
+  sleep 1
+done
+echo ""
 echo "Portas disponíveis no projeto:"
 echo ""
 echo " - NIFI            : 8090/nifi"
